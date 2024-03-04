4344d0bfcffe02ce9470081787a79d0530976514
Joe Herbert <joe@joeherbert.dev>
2024-03-04 15:10:13 +0000

    Support for disney plus

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 ![](logo/fullLogo.svg)
 
-Chrome extension to stop Netflix binging by only letting you watch one episode at a time
+Chrome extension to stop binging by only letting you watch one episode at a time
 
 Firefox doesn't work - waiting on [`window.navigation`](https://developer.mozilla.org/en-US/docs/Web/API/Window/navigation#browser_compatibility) support.
 
-To do:
-- Install, update and uninstall pages on website
-- Enable/disable with keyboard extension and button in extension popup
-- Support for other streaming sites
+## Works for:
+
+-   Netflix
+-   Disney Plus
+
+If the extension doesn't work on either of these then please [email me](mailto:joe@joeherbert.dev?subject=Oneflix).
+
+## To do:
+
+-   Install, update and uninstall pages on website
+-   Enable/disable with keyboard extension and button in extension popup
+-   Support for other streaming sites
--- chrome/oneflix.js
+++ chrome/oneflix.js
@@ -1,13 +1,33 @@
 window.navigation.addEventListener("navigate", (event) => {
-    const windowParams = new URLSearchParams(window.location.search);
-    const eventSearch = new URL(event.destination.url).search;
-    const eventParams = new URLSearchParams(eventSearch);
-    if (
-        event.canIntercept &&
-        window.location.href.includes("watch") &&
-        event.destination.url.includes("watch") &&
-        windowParams.get("trackId") !== eventParams.get("trackId")
-    ) {
-        chrome.runtime.sendMessage({ closeTab: true });
+    if (event.canIntercept) {
+        if (
+            window.location.href.includes("netflix") &&
+            event.destination.url.includes("netflix")
+        ) {
+            const windowParams = new URLSearchParams(window.location.search);
+            const eventSearch = new URL(event.destination.url).search;
+            const eventParams = new URLSearchParams(eventSearch);
+            if (
+                window.location.href.includes("watch") &&
+                event.destination.url.includes("watch") &&
+                windowParams.get("trackId") !== eventParams.get("trackId")
+            ) {
+                chrome.runtime.sendMessage({ closeTab: true });
+            }
+        } else if (
+            window.location.href.includes("disney") &&
+            event.destination.url.includes("disney")
+        ) {
+            let windowUrl = window.location.href.replace(/\/$/, "");
+            let eventUrl = event.destination.url.replace(/\/$/, "");
+            if (
+                window.location.href.includes("video") &&
+                event.destination.url.includes("video") &&
+                windowUrl.substring(windowUrl.lastIndexOf("/") + 1) !==
+                    eventUrl.substring(eventUrl.lastIndexOf("/") + 1)
+            ) {
+                chrome.runtime.sendMessage({ closeTab: true });
+            }
+        }
     }
 });
