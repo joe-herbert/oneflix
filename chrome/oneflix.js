d1806d72ad1d7add40dac1372e5c48303c28af44
Joe Herbert <joe@joeherbert.dev>
2024-03-07 11:56:33 +0000

    Fix not closing on Mac OS

--- chrome/oneflix.js
+++ chrome/oneflix.js
@@ -1,33 +1,25 @@
+let firstWatch = true;
+
 window.navigation.addEventListener("navigate", (event) => {
     if (event.canIntercept) {
-        if (
-            window.location.href.includes("netflix") &&
-            event.destination.url.includes("netflix")
-        ) {
-            const windowParams = new URLSearchParams(window.location.search);
-            const eventSearch = new URL(event.destination.url).search;
-            const eventParams = new URLSearchParams(eventSearch);
-            if (
-                window.location.href.includes("watch") &&
-                event.destination.url.includes("watch") &&
-                windowParams.get("trackId") !== eventParams.get("trackId")
-            ) {
+        let windowPath = window.location.pathname;
+        let eventPath = new URL(event.destination.url).pathname;
+        windowPath = windowPath.replace(/\/$/, "");
+        eventPath = eventPath.replace(/\/$/, "");
+        let differentEpisodes = windowPath.substring(windowPath.lastIndexOf("/") + 1) !== eventPath.substring(eventPath.lastIndexOf("/") + 1);
+
+        let watchString;
+        if (window.location.href.includes("netflix") && event.destination.url.includes("netflix")) {
+            watchString = "watch";
+        } else if (window.location.href.includes("disney") && event.destination.url.includes("disney")) {
+            watchString = "video";
+        }
+
+        if (window.location.href.includes(watchString) && event.destination.url.includes(watchString) && differentEpisodes) {
+            if (!firstWatch) {
                 chrome.runtime.sendMessage({ closeTab: true });
             }
-        } else if (
-            window.location.href.includes("disney") &&
-            event.destination.url.includes("disney")
-        ) {
-            let windowUrl = window.location.href.replace(/\/$/, "");
-            let eventUrl = event.destination.url.replace(/\/$/, "");
-            if (
-                window.location.href.includes("video") &&
-                event.destination.url.includes("video") &&
-                windowUrl.substring(windowUrl.lastIndexOf("/") + 1) !==
-                    eventUrl.substring(eventUrl.lastIndexOf("/") + 1)
-            ) {
-                chrome.runtime.sendMessage({ closeTab: true });
-            }
+            firstWatch = false;
         }
     }
 });
