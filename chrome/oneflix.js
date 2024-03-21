932982cd839f839428e56b218acc6f0a5b71817a
Joe Herbert <joe@joeherbert.dev>
2024-03-21 11:17:26 +0000

    Fix disney+ sometimes using different url, add debugging, fix netflix skipping episode when continuing watching

--- chrome/oneflix.js
+++ chrome/oneflix.js
@@ -1,4 +1,5 @@
-let watched = -1;
+let watched = 0;
+const debug = false;
 
 window.navigation.addEventListener("navigate", (event) => {
     if (event.canIntercept) {
@@ -6,30 +7,67 @@ window.navigation.addEventListener("navigate", (event) => {
         let eventPath = new URL(event.destination.url).pathname;
         windowPath = windowPath.replace(/\/$/, "");
         eventPath = eventPath.replace(/\/$/, "");
-        let differentEpisodes = windowPath.substring(windowPath.lastIndexOf("/") + 1) !== eventPath.substring(eventPath.lastIndexOf("/") + 1);
-
-        let watchString;
-        if (window.location.href.includes("netflix") && event.destination.url.includes("netflix")) {
-            watchString = "watch";
-        } else if (window.location.href.includes("disney") && event.destination.url.includes("disney")) {
-            watchString = "video";
+        let windowURLParam = windowPath.substring(windowPath.lastIndexOf("/") + 1);
+        let eventURLParam = eventPath.substring(eventPath.lastIndexOf("/") + 1);
+        let windowSearchParam = window.location.search === "" ? undefined : new URLSearchParams(window.location.search).get("trackId");
+        let eventUrl = new URL(event.destination.url);
+        let eventSearchParam = eventUrl.search === "" ? undefined : new URLSearchParams(eventUrl.search).get("trackId");
+        let differentEpisodes = windowURLParam !== eventURLParam;
+        if (debug) {
+            console.log("Watched: " + watched);
+            console.log(windowURLParam, eventURLParam, windowSearchParam, eventSearchParam, windowURLParam !== eventURLParam, differentEpisodes);
         }
 
-        if (watchString && window.location.href.includes(watchString) && event.destination.url.includes(watchString) && differentEpisodes) {
+        let watchStrings;
+        if (window.location.href.includes("netflix") && event.destination.url.includes("netflix")) {
+            watchStrings = ["watch"];
+            if (eventPath.includes(watchStrings[0]) && window.location.search === "" && window.location.href.includes("browse")) {
+                watched -= 1;
+            }
+        } else if (window.location.href.includes("disney") && event.destination.url.includes("disney")) {
+            watchStrings = ["video", "play"];
+        }
+
+        if (
+            watchStrings &&
+            watchStrings.reduce((a, c) => a + (window.location.href.includes(c) && event.destination.url.includes(c)), 0) == 1 &&
+            differentEpisodes
+        ) {
+            if (debug) {
+                console.log(
+                    watched,
+                    window.location,
+                    window.location.href,
+                    event.destination,
+                    event.destination.url,
+                    event.downloadRequest,
+                    event.formData,
+                    event.hashChange,
+                    event.navigationType,
+                    event.signal,
+                    event.userInitiated
+                );
+            }
             chrome.storage.sync.get(["toWatch", "enabled"], (values) => {
                 if (values.enabled === undefined) {
                     chrome.storage.sync.set({ enabled: true });
                     values.enabled = true;
                 }
+                console.log(values.enabled);
                 if (values.enabled) {
                     watched++;
                     if (values.toWatch === undefined) {
                         chrome.storage.sync.set({ toWatch: 1 });
                         values.toWatch = 1;
                     }
+                    console.log(watched, values.toWatch);
                     if (watched >= values.toWatch) {
                         watched = -1;
-                        chrome.runtime.sendMessage({ closeTab: true });
+                        if (debug) {
+                            console.log("CLOSE");
+                        } else {
+                            chrome.runtime.sendMessage({ closeTab: true });
+                        }
                     }
                 }
             });
